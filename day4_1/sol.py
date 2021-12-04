0f374e1db981e73f0280ed169eae56c984829a0e
Niklas Dewally <niklas.dewally@gmail.com>
2021-12-04 17:31:41 +0000

    Day 4: Python

---
+++ day4_1/sol.py
@@ -0,0 +1,74 @@
+INPUT = "input"
+BOARD_WIDTH = 5
+
+
+class Board:
+    def __init__(self,values : [str] ):
+        self.board = []
+        self.bingo = False
+        self.crossed = [[0 for i in range(BOARD_WIDTH)] for i in range(BOARD_WIDTH)]
+        for row in range(BOARD_WIDTH):
+            curr_row = []
+            for col in range(BOARD_WIDTH):
+                curr_row.append(values[row*BOARD_WIDTH + col])
+
+            self.board.append(curr_row)
+
+    def check_bingo(self):
+        # row 
+        for row in self.crossed:
+            if sum(row) == BOARD_WIDTH:
+                self.bingo = True
+                return True
+        # column
+        for i in range(BOARD_WIDTH):
+            if sum(row[i] for row in self.crossed) == BOARD_WIDTH:
+                self.bingo = True
+                return True
+        return False
+
+
+    def cross_num(self,n):
+        self.crossed = [[1 if x==n else self.crossed[i][j] for j,x in enumerate(row)] for i,row in enumerate(self.board)]
+        self.check_bingo()
+
+    def __repr__(self):
+        return str(self.board)
+
+
+
+def parse_inputs(filename):
+    bingonums = []   # List of input numbers
+    bingoboards = [] # List of Board objects
+    with open(filename) as f:
+        # First line is bingonums
+        firstline = next(f)
+        bingonums = [int(x) for x in firstline.strip().split(",")]
+        curr_board = []
+        next(f) # skip blank line
+        for i,line in enumerate(f):
+            if line.strip() == "":
+                # New board
+                bingoboards.append(Board(curr_board))
+                curr_board = []
+                continue
+            curr_board += [int(x) for x in line.strip().split(" ") if x != ""]
+
+        # File doesnt end in a whitespace
+        bingoboards.append(Board(curr_board))
+    return bingonums,bingoboards
+
+
+if __name__ == "__main__":
+    nums,boards = parse_inputs(INPUT)
+    i = 0
+    while not any(board.bingo for board in boards):
+        next_num = nums[i]
+        for board in boards:
+            board.cross_num(next_num)
+    
+        i+=1
+
+    winningboard = [board for board in boards if board.bingo][0]
+    sumofunmarked = sum(sum(x if not winningboard.crossed[i][j] else 0 for j,x in enumerate(row)) for i,row in enumerate(winningboard.board))
+    print(sumofunmarked*next_num)
